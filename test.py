48a6b4dac2109bd110e28d83a80cf2e9ce3e5712
aaronlee <aaronlee.uva@gmail.com>
2022-09-19 00:15:53 -0400

    Initial Commit

---
+++ test.py
@@ -0,0 +1,32 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * self.length + 2 * self.width
+
+
+class Square(Rectangle):
+    def __init__(self, length):
+        super(Square, self).__init__(length, length)
+
+
+class Cube(Square):
+    def surface_area(self):
+        face_area = super(Cube, self).area()
+        return face_area * 6
+
+    def volume(self):
+        face_area = super(Cube, self).area()
+        return face_area * self.length
+
+
+cube = Cube(6)
+area = cube.surface_area()
+print(area)
+volume = cube.volume()
+print(volume)
